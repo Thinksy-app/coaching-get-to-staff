129cd9b13efa0a8f9fa476b6acd075e83031207c
edeng <edeng94@gmail.com>
2024-01-13 15:27:24 -0800

    finish coaching part

--- layouts/CoachingLayout.tsx
+++ layouts/CoachingLayout.tsx
@@ -6,6 +6,8 @@ import SocialIcon from '@/components/social-icons'
 import Image from '@/components/Image'
 import Cal, { getCalApi } from "@calcom/embed-react";
 import { useEffect } from "react";
+import Link from '@/components/Link'
+
 
 export function MyCalendar() {
     useEffect(()=>{
@@ -35,13 +37,65 @@ export default function CoachingLayout() {
             </div>
         </div>
 
+        <div>
+            <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                Performance review preparation for high performers
+            </h2>
 
+            <br />
+
+            <p className="text-lg leading-7">
+            If you want to become staff quickly you need to be very intentional with how you spend your time at work. Some of it is intuitive, some is not. <br /><br />
+
+            In our coaching sessions we will plan out exactly what moves you need to make to get high ratings on your next performance review.
+
+            </p>
+
+
+        </div>
+
+        <br /><br />
 
         <div>
             <h2 className="text-2xl font-bold leading-8 tracking-tight">
                 Schedule your consultation today:
             </h2>
+
+            <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            <em>Issues viewing the calendar?&nbsp;
+            <Link href="https://cal.com/edeng/initial-consultation-get-to-staff" className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400" aria-label="All posts">
+                Book directly here.</Link>
+            </em>
+            <br /><br />
+          </p>
                 <MyCalendar />
+        </div>
+
+        <br /><br />
+
+        <div>
+            <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                Pricing
+            </h2>
+
+            <p className="text-lg leading-7">
+            • Free 45min initial consultation<br />
+            • $500 per 1 hour session (recommended: weekly or bi-monthly)<br />
+            • No minimum commitment (recommended: 3 months minimum)<br />
+
+            </p>
+
+        </div>
+
+        <br /><br />
+
+        <div className="flex flex-col justify-center items-center text-base font-medium leading-6">
+          <p className="text-lg leading-7">Subscribe for tips every Monday!</p>
+          <iframe
+            src="https://www.gettostaff.com/embed"
+            width={480}
+            height={150}
+          ></iframe>
         </div>
 
     </div>
