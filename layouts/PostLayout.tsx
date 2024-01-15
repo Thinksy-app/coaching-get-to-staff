1c5929564d8518ad1ad69f14bccb07c11fa85867
edeng <edeng94@gmail.com>
2024-01-15 13:36:48 -0800

    add subscribe to below all newsletters

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -93,6 +93,13 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
             </dl>
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
               <div className="prose max-w-none pb-8 pt-10 dark:prose-invert">{children}</div>
+              <div className="flex justify-center text-base font-medium leading-6">
+                <iframe
+                  src="https://entreeden.substack.com/embed"
+                  width={480}
+                  height={150}
+                ></iframe>
+              </div>
             </div>
             <footer>
               <div className="divide-gray-200 text-sm font-medium leading-5 dark:divide-gray-700 xl:col-start-1 xl:row-start-2 xl:divide-y">
