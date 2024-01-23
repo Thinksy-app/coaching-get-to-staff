649d287231fadec2b57f7935ee08e01b7ea93c37
edeng <edeng94@gmail.com>
2024-01-22 16:25:44 -0800

    add some testimonials

--- layouts/CoachingLayout.tsx
+++ layouts/CoachingLayout.tsx
@@ -24,6 +24,126 @@ export function MyCalendar() {
     />;
 };
 
+export function Testimonials() {
+    return (
+        <>
+        <div className="min-w-screen flex items-center justify-center py-5">
+            <div className="w-full border-t border-b px-5 py-16 md:py-4">
+                <div className="w-full max-w-6xl mx-auto">
+                    <div className="-mx-3 md:flex items-start">
+                        <div className="px-3 md:w-1/3">
+                            <div className="w-full mx-auto rounded-lg  border border-gray-200 p-5  font-light mb-6">
+                                <div className="w-full flex mb-4 items-center">
+                                    <div className="overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200">
+                                    <Image width={38} height={38} src="/static/images/testimonials/micho.jpeg" alt=""/>
+                                    </div>
+                                    <div className="flex-grow pl-3">
+                                        <Link href="https://www.linkedin.com/in/michel-perez-41790939/" className="text-secondary-500 hover:text-primary-600 dark:hover:text-primary-400" aria-label="Michel P. Linkedin"><h6 className="font-bold text-sm uppercase">Michel P.</h6></Link>
+                                    </div>
+                                </div>
+                                <div className="w-full">
+                                    <p className="text-sm leading-tight">
+                                        <span className="text-lg leading-none italic font-bold  mr-1">&ldquo;</span>
+                                        Eden offers perspectives that are refreshingly unique, coupled with amazing suggestions on how to maximize impact in any given task or goal. <br /> <br />
+
+                                        <strong>He has an exceptional ability to identify what's the best use of your time and the key elements to focus on.</strong> <br /> <br />
+
+                                        Eden isn't just an excellent engineer; he's a career catalyst.
+                                        <span className="text-lg leading-none italic font-bold  ml-1">"</span></p>
+                                </div>
+                            </div>
+                            {/* <div className="w-full mx-auto rounded-lg  border border-gray-200 p-5  font-light mb-6">
+                                <div className="w-full flex mb-4 items-center">
+                                    <div className="overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200">
+                                        <Image width={38} height={38} src="/static/images/testimonials/emilio.jpeg" alt=""/>
+                                    </div>
+                                    <div className="flex-grow pl-3">
+                                        <Link href="https://www.linkedin.com/in/emilioaurea/" className="text-secondary-500 hover:text-primary-600 dark:hover:text-primary-400" aria-label="Emilio A. Linkedin"><h6 className="font-bold text-sm uppercase">Emilio A.</h6></Link>
+                                    </div>
+                                </div>
+                                <div className="w-full">
+                                    <p className="text-sm leading-tight"><span className="text-lg leading-none italic font-bold  mr-1">&ldquo;</span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto inventore voluptatum nostrum atque, corrupti, vitae esse id accusamus dignissimos neque reprehenderit natus, hic sequi itaque dicta nisi voluptatem! Culpa, iusto.<span className="text-lg leading-none italic font-bold  ml-1">"</span></p>
+                                </div>
+                            </div> */}
+                        </div>
+                        <div className="px-3 md:w-1/3">
+                            <div className="w-full mx-auto rounded-lg  border border-gray-200 p-5  font-light mb-6">
+                                <div className="w-full flex mb-4 items-center">
+                                    <div className="overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200">
+                                        <Image width={38} height={38} src="/static/images/testimonials/caleb.jpeg" alt=""/>
+                                    </div>
+                                    <div className="flex-grow pl-3">
+                                        <Link href="https://www.linkedin.com/in/calebghirmai/" className="text-secondary-500 hover:text-primary-600 dark:hover:text-primary-400" aria-label="Caleb G. Linkedin"><h6 className="font-bold text-sm uppercase">Caleb G.</h6></Link>
+                                    </div>
+                                </div>
+                                <div className="w-full">
+                                    <p className="text-sm leading-tight">
+                                        <span className="text-lg leading-none italic font-bold  mr-1">&ldquo;</span>
+                                        Before Eden's career coaching, I struggled with technical and behavioral interviews. Eden planned a system for me that helped me revisit a variety of questions and traits employers seek in their candidates. <br /><br />
+
+                                        <strong>Most importantly, he instilled a level of confidence in myself that made me truly believe I will become a great Software Engineer.</strong>
+                                        {/* I highly recommend Eden's services because he is a great listener, empathetic, and provides practical advice that can be immediately applied in the technical field! */}
+                                        <span className="text-lg leading-none italic font-bold  ml-1">"</span></p>
+                                </div>
+                            </div>
+                            {/* <div className="w-full mx-auto rounded-lg  border border-gray-200 p-5  font-light mb-6">
+                                <div className="w-full flex mb-4 items-center">
+                                    <div className="overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200">
+                                        <Image width={38} height={38} src="/static/images/testimonials/caleb.jpeg" alt=""/>
+                                    </div>
+                                    <div className="flex-grow pl-3">
+                                    <Link href="https://www.linkedin.com/in/michel-perez-41790939/" className="text-secondary-500 hover:text-primary-600 dark:hover:text-primary-400" aria-label="Michel P. Linkedin"><h6 className="font-bold text-sm uppercase">Michel P.</h6></Link>
+                                    </div>
+                                </div>
+                                <div className="w-full">
+                                    <p className="text-sm leading-tight"><span className="text-lg leading-none italic font-bold  mr-1">&ldquo;</span>Lorem ipsum, dolor sit amet, consectetur adipisicing elit. Dolore quod necessitatibus, labore sapiente, est, dignissimos ullam error ipsam sint quam tempora vel.<span className="text-lg leading-none italic font-bold  ml-1">"</span></p>
+                                </div>
+                            </div> */}
+                        </div>
+
+                        <div className="px-3 md:w-1/3">
+                            <div className="w-full mx-auto rounded-lg  border border-gray-200 p-5  font-light mb-6">
+                                <div className="w-full flex mb-4 items-center">
+                                    <div className="overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200">
+                                    <Image width={38} height={38} src="/static/images/testimonials/jessica.jpeg" alt=""/>
+                                    </div>
+                                    <div className="flex-grow pl-3">
+                                    <Link href="https://www.linkedin.com/in/jessica-livak/" className="text-secondary-500 hover:text-primary-600 dark:hover:text-primary-400" aria-label="Jessica L. Linkedin"><h6 className="font-bold text-sm uppercase">Jessica L.</h6></Link>
+                                    </div>
+                                </div>
+                                <div className="w-full">
+                                    <p className="text-sm leading-tight">
+                                        <span className="text-lg leading-none italic font-bold  mr-1">&ldquo;</span>
+                                        Eden’s exceptional guidance and mentorship have been instrumental in helping me break through career obstacles and achieve my goals. <br /> <br />
+
+                                        <strong>He has a talent for breaking down complex skills into practical, actionable steps, enabling me to have a clear path towards my goals.</strong> <br /> <br />
+
+                                        Working with Eden has been a game-changer for my career, and I couldn’t be more grateful for his expertise and support.
+                                        <span className="text-lg leading-none italic font-bold  ml-1">"</span></p>
+                                </div>
+                            </div>
+                            {/* <div className="w-full mx-auto rounded-lg  border border-gray-200 p-5  font-light mb-6">
+                                <div className="w-full flex mb-4 items-center">
+                                    <div className="overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200">
+                                        <Image width={38} height={38} src="https://i.pravatar.cc/100?img=2" alt=""/>
+                                    </div>
+                                    <div className="flex-grow pl-3">
+                                        <h6 className="font-bold text-sm uppercase ">Kris Stanton.</h6>
+                                    </div>
+                                </div>
+                                <div className="w-full">
+                                    <p className="text-sm leading-tight"><span className="text-lg leading-none italic font-bold  mr-1">&ldquo;</span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem iusto, explicabo, cupiditate quas totam!<span className="text-lg leading-none italic font-bold  ml-1">"</span></p>
+                                </div>
+                            </div> */}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+
 export default function CoachingLayout() {
 
   return (
@@ -61,7 +181,7 @@ export default function CoachingLayout() {
                 Schedule your consultation today:
             </h2>
 
-            <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            <p className="text-lg leading-7 text-gray-500 dark:">
             <em>Issues viewing the calendar?&nbsp;
             <Link href="https://cal.com/edeng/initial-consultation-get-to-staff" className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400" aria-label="All posts">
                 Book directly here.</Link>
@@ -85,6 +205,15 @@ export default function CoachingLayout() {
 
             </p>
 
+        </div>
+
+        <br /><br />
+
+        <div>
+            <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                Testimonials
+            </h2>
+            <Testimonials />
         </div>
 
         <br /><br />
