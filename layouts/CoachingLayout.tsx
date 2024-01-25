0eff4a19ec9b09a1cb2bedb2b5e868a337e18f76
edeng <edeng94@gmail.com>
2024-01-24 16:35:42 -0800

    add new testimonial

--- layouts/CoachingLayout.tsx
+++ layouts/CoachingLayout.tsx
@@ -52,19 +52,25 @@ export function Testimonials() {
                                         <span className="text-lg leading-none italic font-bold  ml-1">"</span></p>
                                 </div>
                             </div>
-                            {/* <div className="w-full mx-auto rounded-lg  border border-gray-200 p-5  font-light mb-6">
+                            <div className="w-full mx-auto rounded-lg  border border-gray-200 p-5  font-light mb-6">
                                 <div className="w-full flex mb-4 items-center">
                                     <div className="overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200">
-                                        <Image width={38} height={38} src="/static/images/testimonials/emilio.jpeg" alt=""/>
+                                        <Image width={38} height={38} src="/static/images/testimonials/eduardo.jpeg" alt=""/>
                                     </div>
                                     <div className="flex-grow pl-3">
-                                        <Link href="https://www.linkedin.com/in/emilioaurea/" className="text-secondary-500 hover:text-primary-600 dark:hover:text-primary-400" aria-label="Emilio A. Linkedin"><h6 className="font-bold text-sm uppercase">Emilio A.</h6></Link>
+                                    <Link href="https://www.linkedin.com/in/eduardo-jcu/" className="text-secondary-500 hover:text-primary-600 dark:hover:text-primary-400" aria-label="Eudardo C. Linkedin"><h6 className="font-bold text-sm uppercase">Eduardo C.</h6></Link>
                                     </div>
                                 </div>
                                 <div className="w-full">
-                                    <p className="text-sm leading-tight"><span className="text-lg leading-none italic font-bold  mr-1">&ldquo;</span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto inventore voluptatum nostrum atque, corrupti, vitae esse id accusamus dignissimos neque reprehenderit natus, hic sequi itaque dicta nisi voluptatem! Culpa, iusto.<span className="text-lg leading-none italic font-bold  ml-1">"</span></p>
+                                    <p className="text-sm leading-tight"><span className="text-lg leading-none italic font-bold  mr-1">&ldquo;</span>
+                                    Over the years Eden has given me invaluable personal advice for handling different work situations - from working on promotion packets, all the way to considering new teams or opportunities.  <br /> <br />
+
+                                    <strong>Eden has the great ability of understanding your situation and making personalized suggestions that fit your goals and help advance your career.</strong> <br /> <br />
+
+                                    He is a very candid and pragmatic engineer that has helped me focus on the things that are ultimately important.
+                                    <span className="text-lg leading-none italic font-bold  ml-1">"</span></p>
                                 </div>
-                            </div> */}
+                            </div>
                         </div>
                         <div className="px-3 md:w-1/3">
                             <div className="w-full mx-auto rounded-lg  border border-gray-200 p-5  font-light mb-6">
@@ -89,14 +95,14 @@ export function Testimonials() {
                             {/* <div className="w-full mx-auto rounded-lg  border border-gray-200 p-5  font-light mb-6">
                                 <div className="w-full flex mb-4 items-center">
                                     <div className="overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200">
-                                        <Image width={38} height={38} src="/static/images/testimonials/caleb.jpeg" alt=""/>
+                                        <Image width={38} height={38} src="/static/images/testimonials/emilio.jpeg" alt=""/>
                                     </div>
                                     <div className="flex-grow pl-3">
-                                    <Link href="https://www.linkedin.com/in/michel-perez-41790939/" className="text-secondary-500 hover:text-primary-600 dark:hover:text-primary-400" aria-label="Michel P. Linkedin"><h6 className="font-bold text-sm uppercase">Michel P.</h6></Link>
+                                        <Link href="https://www.linkedin.com/in/emilioaurea/" className="text-secondary-500 hover:text-primary-600 dark:hover:text-primary-400" aria-label="Emilio A. Linkedin"><h6 className="font-bold text-sm uppercase">Emilio A.</h6></Link>
                                     </div>
                                 </div>
                                 <div className="w-full">
-                                    <p className="text-sm leading-tight"><span className="text-lg leading-none italic font-bold  mr-1">&ldquo;</span>Lorem ipsum, dolor sit amet, consectetur adipisicing elit. Dolore quod necessitatibus, labore sapiente, est, dignissimos ullam error ipsam sint quam tempora vel.<span className="text-lg leading-none italic font-bold  ml-1">"</span></p>
+                                    <p className="text-sm leading-tight"><span className="text-lg leading-none italic font-bold  mr-1">&ldquo;</span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto inventore voluptatum nostrum atque, corrupti, vitae esse id accusamus dignissimos neque reprehenderit natus, hic sequi itaque dicta nisi voluptatem! Culpa, iusto.<span className="text-lg leading-none italic font-bold  ml-1">"</span></p>
                                 </div>
                             </div> */}
                         </div>
