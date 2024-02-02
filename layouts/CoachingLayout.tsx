bd9bc6ffce223e385b93487b8d838c9cb4108d0d
edeng <edeng94@gmail.com>
2024-02-01 16:18:14 -0800

    one more testimonial

--- layouts/CoachingLayout.tsx
+++ layouts/CoachingLayout.tsx
@@ -71,6 +71,25 @@ export function Testimonials() {
                                     <span className="text-lg leading-none italic font-bold  ml-1">"</span></p>
                                 </div>
                             </div>
+                            <div className="w-full mx-auto rounded-lg  border border-gray-200 p-5  font-light mb-6">
+                                <div className="w-full flex mb-4 items-center">
+                                    <div className="overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200">
+                                        <Image width={38} height={38} src="/static/images/testimonials/ahmed.jpeg" alt=""/>
+                                    </div>
+                                    <div className="flex-grow pl-3">
+                                    <Link href="https://www.linkedin.com/in/ahmedhawas/" className="text-secondary-500 hover:text-primary-600 dark:hover:text-primary-400" aria-label="Ahmed H. Linkedin"><h6 className="font-bold text-sm uppercase">Ahmed H.</h6></Link>
+                                    </div>
+                                </div>
+                                <div className="w-full">
+                                    <p className="text-sm leading-tight"><span className="text-lg leading-none italic font-bold  mr-1">&ldquo;</span>
+                                    I had the privilege of being mentored by Eden, and I can't recommend him highly enough. Eden is an exceptional mentor who not only identifies growth opportunities effortlessly but also makes learning an enjoyable experience. <br /><br />
+
+                                    <strong>Our 1:1 sessions were a highlight for me as I got to learn something new every single time. <br /><br /></strong>
+
+                                    Eden's guidance not only helped me navigate my technical and career path but also allowed me to bounce ideas off him with ease. His empathy and genuine care for my success were evident in every interaction. If you're seeking a mentor who combines technical expertise with an enjoyable learning journey, Eden is the perfect choice.
+                                    <span className="text-lg leading-none italic font-bold  ml-1">"</span></p>
+                                </div>
+                            </div>
                         </div>
                         <div className="px-3 md:w-1/3">
                             <div className="w-full mx-auto rounded-lg  border border-gray-200 p-5  font-light mb-6">
