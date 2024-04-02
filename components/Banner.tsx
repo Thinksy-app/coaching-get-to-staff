91a8c38ff51647bdce1b3e0f49d786598cf1f229
Eden Ghirmai <edeng94@gmail.com>
2024-04-02 12:03:15 -0700

    add newsletters and update banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -29,19 +29,19 @@ export default function Example() {
       </div>
       <div className="flex flex-wrap items-center gap-x-4 gap-y-2">
         <p className="text-sm leading-6 text-gray-900">
-          <strong className="font-semibold">What's your next step?</strong>
+          <strong className="font-semibold">Ready to level up?</strong>
           <svg viewBox="0 0 2 2" className="mx-2 inline h-0.5 w-0.5 fill-current" aria-hidden="true">
             <circle cx={1} cy={1} r={1} />
           </svg>
-          Take our FREE career journey quiz
+          Join our exclusive private community
         </p>
         <a
-          href="https://form.jotform.com/240527554383156"
+          href="/newsletter/get-to-staff-private-community"
           target="_blank"
           rel="noopener noreferrer"
           className="flex-none rounded-full bg-gray-900 px-3.5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
         >
-          Get Started <span aria-hidden="true">&rarr;</span>
+          Learn More <span aria-hidden="true">&rarr;</span>
         </a>
       </div>
       <div className="flex flex-1 justify-end">
