3c0b1795731faf4feba2d2a8c7c0ded8f4f9bc23
Eden Ghirmai <edeng94@gmail.com>
2024-04-03 14:20:33 -0700

    fix title

--- app/dtech/page.tsx
+++ app/dtech/page.tsx
@@ -4,7 +4,7 @@ import CollegeLayout from '@/layouts/CollegeLayout'
 import { coreContent } from 'pliny/utils/contentlayer'
 import { genPageMetadata } from 'app/seo'
 
-export const metadata = genPageMetadata({ title: 'Coaching' })
+export const metadata = genPageMetadata({ title: 'Get to Staff x DTech' })
 
 export default function Page() {
   const author = allAuthors.find((p) => p.slug === 'default') as Authors
