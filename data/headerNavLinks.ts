db9bdc6ab136923eb4098cd9f1b9c526e86e55ca
Eden Ghirmai <edeng94@gmail.com>
2024-07-22 13:57:20 -0700

    remove course

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,6 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/coaching', title: 'Coaching' },
   { href: '/newsletter', title: 'Newsletter' },
-  { href: '/course', title: 'Course' },
   { href: '/about', title: 'About' },
 ]
 
